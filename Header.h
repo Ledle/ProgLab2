978e3f77415b713f8d0cfaeebde148e29c57261a
Ledle <trojangoblin@gmail.com>
2021-09-12 10:37:13 +0700

    Commented and prototype

--- Header.h
+++ Header.h
@@ -2,22 +2,37 @@
 typedef struct user {
 	int login,password;
 	char* name;
+	group* group;
 };
 typedef struct discipline {
 	test* test;
-	double* multiplier;
+	double* multiplier; //коэффициенты тестов для подсчёта рейтинга
+	group** groups;
 };
 typedef struct group {
-	user* member;
-	discipline* disciplines;
+	user** students;
+	discipline** disciplines;
 };
 typedef struct question {
-	char* text,answer;
-	int value;
+	const char* text,answer;
+	int value;//количество баллов за вопрос
 };
 typedef struct test {
 	question* question;
-	int* result;
-	discipline* discipline;
+	int* result;//результаты пользователей
+	discipline** discipline;
 };
 
+int starttest(test test, user student);//maybe
+user makeuser(int login, int password, group group, const char* name);//создание пользователя
+discipline makediscip(test* tests, double* multiplier);//создание дисциплины
+question makequestion(const char* text, const char* answer, int value);//создание вопроса
+test maketest(question questions[]);//создание теста
+bool addgroup(discipline* disc, group* group);//добавление группы в дисциплину
+bool adduser(group* group, user students[], int n = 1);//добавление пользователя в группу
+int addtest(discipline* disc, test* test,int multiplier);//добавление теста в дисциплину
+int addresult(test* test, int login, int result);//запись результата пользователя
+void showgroup(group group);
+void showdiscip(discipline disc);
+void showtest(test test);
+void showuser(user student);
